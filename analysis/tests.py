5c3ad33fe90ea07bb289fb338b3ec577c8ebd43c
poldrack <poldrack@gmail.com>
2019-07-21 08:24:55 -0700

    style fixes

--- analysis/tests.py
+++ analysis/tests.py
@@ -11,13 +11,14 @@ from narps import Narps
 from AnalyzeMaps import mk_overlap_maps
 from PrepareMetadata import prepare_metadata
 
-# use a session-scoped fixture to 
-# save data for entire session
 
+# use a session-scoped fixture to
+# save data for entire session
 @pytest.fixture(scope="session")
 def basedir(tmpdir_factory):
     fn = tmpdir_factory.mktemp("data")
     return fn
+
 
 @pytest.fixture(scope="session")
 def narps(basedir):
@@ -27,41 +28,53 @@ def narps(basedir):
 
 # tests
 
+
 def test_narps_get_binarized_thresh_masks(narps):
     narps.get_binarized_thresh_masks()
 
+
 def test_narps_get_resampled_images(narps):
     narps.get_resampled_images()
+
 
 def test_narps_create_concat_images_thresh(narps):
     narps.create_concat_images(datatype='resampled',
                                imgtypes=['thresh'])
 
+
 def test_narps_check_image_values(narps):
-    image_metadata_df = narps.check_image_values()
+    _ = narps.check_image_values()
+
 
 def test_narps_create_rectified_images(narps):
     narps.create_rectified_images()
 
+
 def test_narps_create_thresh_overlap_images(narps):
     narps.create_thresh_overlap_images()
 
+
 def test_narps_convert_to_zscores(narps):
     narps.convert_to_zscores()
+
 
 def test_narps_create_concat_images_unthresh(narps):
     narps.create_concat_images(datatype='zstat',
                                imgtypes=['unthresh'])
 
+
 def test_narps_compute_image_stats(narps):
     narps.compute_image_stats()
+
 
 def test_narps_estimate_smoothness(narps):
     _ = narps.estimate_smoothness()
 
+
 def test_prepare_metadata(narps):
     prepare_metadata(narps)
-    
+
+
 def test_analyze_maps(narps):
     # Load full metadata and put into narps structure
     narps.metadata = pandas.read_csv(
