fcd70759d60fa9b0ab66c3b72911984daebf78e4
poldrack <poldrack@gmail.com>
2019-07-21 09:28:59 -0700

    add tests for AnalyzeMaps and ConsensusAnalysis

--- analysis/tests.py
+++ analysis/tests.py
@@ -1,16 +1,16 @@
-"""
-tests for narps code
-- currently these are all just smoke tests
-"""
+# tests for narps code
+# - currently these are all just smoke tests
 
-# contents of conftest.py
 import pytest
 import pandas
 import os
 from narps import Narps
-from AnalyzeMaps import mk_overlap_maps
+from AnalyzeMaps import mk_overlap_maps,\
+    mk_range_maps, mk_std_maps,\
+    mk_correlation_maps_unthresh, analyze_clusters,\
+    plot_distance_from_mean, get_thresh_similarity
 from PrepareMetadata import prepare_metadata
-
+from ConsensusAnalysis import run_ttests, mk_figures
 
 # use a session-scoped fixture to
 # save data for entire session
@@ -73,12 +73,57 @@ def test_narps_estimate_smoothness(narps):
 
 def test_prepare_metadata(narps):
     prepare_metadata(narps)
-
-
-def test_analyze_maps(narps):
     # Load full metadata and put into narps structure
     narps.metadata = pandas.read_csv(
         os.path.join(narps.dirs.dirs['metadata'], 'all_metadata.csv'))
 
+
+def test_mk_overlap_maps(narps):
     # create maps showing overlap of thresholded images
     mk_overlap_maps(narps)
+
+
+def test_mk_range_maps(narps):
+    mk_range_maps(narps)
+
+
+def test_mk_std_maps(narps):
+    mk_std_maps(narps)
+
+
+def test_unthresh_correlation_analysis(narps):
+    # conbine these into a single test
+    # since they share data
+    corr_type = 'spearman'
+    dendrograms, membership = mk_correlation_maps_unthresh(
+        narps, corr_type=corr_type)
+
+    _ = analyze_clusters(
+        narps,
+        dendrograms,
+        membership,
+        corr_type=corr_type)
+
+
+def test_plot_distance_from_mean(narps):
+    plot_distance_from_mean(narps)
+
+
+def test_get_thresh_similarity(narps):
+    get_thresh_similarity(narps)
+
+
+def test_consensus_analysis(narps):
+    logfile = os.path.join(
+        narps.dirs.dirs['logs'],
+        'ConsensusAnalysis.txt')
+
+    narps.dirs.dirs['consensus'] = os.path.join(
+        narps.dirs.dirs['output'],
+        'consensus_analysis')
+
+    if not os.path.exists(narps.dirs.dirs['consensus']):
+        os.mkdir(narps.dirs.dirs['consensus'])
+
+    run_ttests(narps, logfile)
+    mk_figures(narps, logfile)
