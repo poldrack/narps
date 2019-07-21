1b63d739ad016641feff0952f86ac384936f3066
poldrack <poldrack@gmail.com>
2019-07-21 08:20:11 -0700

    use narps fixture

--- analysis/tests.py
+++ analysis/tests.py
@@ -19,78 +19,50 @@ def basedir(tmpdir_factory):
     fn = tmpdir_factory.mktemp("data")
     return fn
 
+@pytest.fixture(scope="session")
+def narps(basedir):
+    narps = Narps(basedir)
+    narps.write_data()
+    return(narps)
+
 # tests
-def test_narps_main_class(basedir):
-    narps = Narps(basedir)
-    narps.write_data()
 
-def test_narps_get_binarized_thresh_masks(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_get_binarized_thresh_masks(narps):
     narps.get_binarized_thresh_masks()
-    narps.write_data()
 
-def test_narps_get_resampled_images(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_get_resampled_images(narps):
     narps.get_resampled_images()
-    narps.write_data()
 
-def test_narps_create_concat_images_thresh(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_create_concat_images_thresh(narps):
     narps.create_concat_images(datatype='resampled',
                                imgtypes=['thresh'])
 
-def test_narps_check_image_values(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_check_image_values(narps):
     image_metadata_df = narps.check_image_values()
 
-def test_narps_create_rectified_images(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_create_rectified_images(narps):
     narps.create_rectified_images()
-    narps.write_data()
 
-def test_narps_create_thresh_overlap_images(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_create_thresh_overlap_images(narps):
     narps.create_thresh_overlap_images()
 
-def test_narps_convert_to_zscores(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_convert_to_zscores(narps):
     narps.convert_to_zscores()
-    narps.write_data()
 
-def test_narps_create_concat_images_unthresh(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_create_concat_images_unthresh(narps):
     narps.create_concat_images(datatype='zstat',
                                imgtypes=['unthresh'])
 
-def test_narps_compute_image_stats(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_compute_image_stats(narps):
     narps.compute_image_stats()
 
-def test_narps_estimate_smoothness(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_narps_estimate_smoothness(narps):
     _ = narps.estimate_smoothness()
-    narps.write_data()
 
-def test_prepare_metadata(basedir):
-    narps = Narps(basedir)
-    narps.load_data()
+def test_prepare_metadata(narps):
     prepare_metadata(narps)
     
-def test_analyze_maps(basedir):
-    # setup main class
-    narps = Narps(basedir)
-    narps.load_data()
-
+def test_analyze_maps(narps):
     # Load full metadata and put into narps structure
     narps.metadata = pandas.read_csv(
         os.path.join(narps.dirs.dirs['metadata'], 'all_metadata.csv'))
