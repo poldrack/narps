f7d9fc9dba0232e7878924cf27fe171daedb848c
poldrack <poldrack@gmail.com>
2019-07-21 11:23:17 -0700

    fix style

--- analysis/tests_minimal.py
+++ analysis/tests_minimal.py
@@ -2,17 +2,7 @@
 # - minimal set for configuration, not for production use
 
 import pytest
-import pandas
-import os
 from narps import Narps
-from AnalyzeMaps import mk_overlap_maps,\
-    mk_range_maps, mk_std_maps,\
-    mk_correlation_maps_unthresh, analyze_clusters,\
-    plot_distance_from_mean, get_thresh_similarity
-from PrepareMetadata import prepare_metadata
-from ConsensusAnalysis import run_ttests, mk_figures
-from MakeSupplementaryFigure1 import mk_supp_figure1,\
-    get_all_metadata
 
 # use a session-scoped fixture to
 # save data for entire session
