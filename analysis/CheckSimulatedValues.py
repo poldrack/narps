6536a6366c5470822a949422b0d642bbedcdbea2
poldrack <poldrack@gmail.com>
2019-07-22 19:02:23 -0700

    add logging

--- analysis/CheckSimulatedValues.py
+++ analysis/CheckSimulatedValues.py
@@ -7,6 +7,7 @@ import os
 import numpy
 import nilearn.input_data
 from narps import Narps, NarpsDirs, hypnums # noqa, flake8 error
+from utils import log_to_file
 
 thresh = 0.95
 basedir = os.environ['NARPS_BASEDIR']
@@ -34,7 +35,9 @@ for hyp in hypnums:
 
 try:
     assert numpy.array(list(cc.values())).min() > thresh
-    print('SUCESSS: all hypotheses correlated > %0.2f' % thresh)
+    log_to_file(os.path.join(simdir, 'logs/simulated_data.log'),
+        'SUCESSS: all hypotheses correlated > %0.2f' % thresh)
 except AssertionError:
-    print('FAILURE')
+    log_to_file(os.path.join(simdir, 'logs/simulated_data.log'),
+        'FAILURE')
     print(cc)
