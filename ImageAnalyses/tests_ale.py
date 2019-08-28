7dde0859498ef0a224737569e1ab8e4bc3dc7802
poldrack <poldrack@gmail.com>
2019-08-27 18:17:07 -0700

    add ALE:

---
+++ ImageAnalyses/tests_ale.py
@@ -0,0 +1,43 @@
+# tests for narps code
+# - currently these are all just smoke tests
+
+import pytest
+import os
+from narps import Narps
+from MetaAnalysis import get_thresholded_Z_maps,\
+    extract_peak_coordinates, run_ALE, save_results,\
+    make_figures, make_combined_figure
+# Use a fixed base dir so that we can
+# access the results as a circleci artifact
+
+
+@pytest.fixture(scope="session")
+def narps():
+    basedir = '/tmp/data'
+    assert os.path.exists(basedir)
+    narps = Narps(basedir)
+    narps.load_data()
+    return(narps)
+
+
+# tests
+# run ALE meta-analysis
+def test_run_ALE(narps):
+    _ = narps.dirs.get_output_dir('ALE')
+
+    # create thresholded versions of Z maps
+    narps = get_thresholded_Z_maps(
+        narps)
+
+    # extract peak coordinates
+    for hyp in range(1, 10):
+        ds_dict = extract_peak_coordinates(
+            narps,
+            hyp)
+
+        # Performing ALE
+        res = run_ALE(ds_dict, hyp)
+        images = save_results(hyp, res, narps)
+        make_figures(narps, hyp, images)
+    # make a figure with all hypotheses
+    make_combined_figure(narps)
