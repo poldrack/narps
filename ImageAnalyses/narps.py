be0436a4caeda192243b816aeb9fec16e4fd103b
poldrack <poldrack@gmail.com>
2019-08-28 21:32:38 -0700

    remove outdated flipping

--- ImageAnalyses/narps.py
+++ ImageAnalyses/narps.py
@@ -75,11 +75,6 @@ hypotheses = {1: '+gain: equal indiff',
 
 hypnums = [1, 2, 5, 6, 7, 8, 9]
 
-# one team had thresholded maps that
-# had 0 for exceedence and 1 for null
-# so we flip those
-FLIP_THRESH_MAPS = {'27SS': [2, 5, 7]}
-
 
 # separate class to store base directories,
 # since we need them in multiple places
@@ -320,10 +315,7 @@ class NarpsTeam(object):
                 if replace_na:
                     threshdata = numpy.nan_to_num(threshdata)
                 threshdata_bin = numpy.zeros(threshdata.shape)
-                # fix teams with maps where 1 is null and zero is supra
-                if self.teamID in FLIP_THRESH_MAPS:
-                    if hyp in FLIP_THRESH_MAPS[self.teamID]:
-                        threshdata_bin = -1 * (threshdata_bin - 1)
+
                 # if the team reported using a negative contrast,
                 # then we use the negative direction, otherwise
                 # use the positive direction.
