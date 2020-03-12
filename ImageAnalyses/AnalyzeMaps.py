65097064f17b1473860f8af8f3851cd9e5b0b59a
poldrack <poldrack@gmail.com>
2020-03-12 09:37:02 -0700

    convert idx to list

--- ImageAnalyses/AnalyzeMaps.py
+++ ImageAnalyses/AnalyzeMaps.py
@@ -382,8 +382,9 @@ def mk_correlation_maps_unthresh(
             membership[str(hyp)][str(cl)].append(labels[j])
 
         # save reordered correlation map
-        reordered_df = df.loc[cm.dendrogram_row.reordered_ind, :]
-        reordered_df = reordered_df.loc[:, cm.dendrogram_row.reordered_ind]
+        reord_idx = [j for j in cm.dendrogram_row.reordered_ind]
+        reordered_df = df.loc[reord_idx, :]
+        reordered_df = reordered_df.loc[:, reord_idx]
         reordered_df.to_csv(os.path.join(
             output_dir,
             '%s_unthresh_hyp%d_reordered.csv' % (corr_type, hyp)))
