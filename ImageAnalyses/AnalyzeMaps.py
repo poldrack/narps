cac3e84656b57df1162f6b90ffd644580a01b409
poldrack <poldrack@gmail.com>
2020-03-12 08:54:51 -0700

    save reoedered corr matrices

--- ImageAnalyses/AnalyzeMaps.py
+++ ImageAnalyses/AnalyzeMaps.py
@@ -381,6 +381,13 @@ def mk_correlation_maps_unthresh(
                 membership[str(hyp)][str(cl)] = []
             membership[str(hyp)][str(cl)].append(labels[j])
 
+        # save reordered correlation map
+        reordered_df = df.loc[cm.dendrogram_row.reordered_ind, :]
+        reordered_df = reordered_df.loc[:, cm.dendrogram_row.reordered_ind]
+        reordered_df.to_csv(os.path.join(
+            output_dir,
+            '%s_unthresh_hyp%d_reordered.csv' % (corr_type, hyp)))
+
     # save cluster data to file so that we don't have to rerun everything
     with open(os.path.join(
               output_dir,
